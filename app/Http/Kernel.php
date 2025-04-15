ffc9bb077b76a877d1740274bce5c083a2f0dc59
Lucas Gournay <133283871+Spydertnt@users.noreply.github.com>
2025-04-15 22:26:25 +0200

    Add files via upload

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,6 +31,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\LogUserActivity::class,
+            \App\Http\Middleware\TrackUserActions::class,
         ],
 
         'api' => [
@@ -59,4 +60,4 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
-} 
+}
